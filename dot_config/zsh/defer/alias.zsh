292eef08f39ea17f7dff003d78ce6306e9692ded
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-06-14 23:16:05 +0900

    ✨ chezmoiで管理

---
+++ dot_config/zsh/defer/alias.zsh
@@ -0,0 +1,13 @@
+
+if command -v code-insiders >/dev/null 2>&1; then
+  alias code="code-insiders"
+fi
+
+# git alias
+alias gc="git clone"
+
+# docker alias
+alias d="docker"
+alias dc="docker compose"
+alias dcu="docker compose up"
+alias dcd="docker compose down"
