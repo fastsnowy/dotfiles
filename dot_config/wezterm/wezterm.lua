af63871b10f53e521d58ab94377002757f22e222
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-07-09 00:55:52 +0900

    ✨ Add wezterm configuration file

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+local config = {}
+
+config.font = wezterm.font "HackGen35 Console NF"
+config.color_scheme = 'Tokyo Night'
+config.font_size = 18
+
+return config
