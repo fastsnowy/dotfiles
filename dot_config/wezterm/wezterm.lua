99fccfc2be4d823dc0b00714411c8a10a0ad5ce4
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-07-17 03:11:46 +0900

    ⚡️ enable automatic config reload in wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm';
 local config = {}
 
+config.automatically_reload_config = true
 config.font = wezterm.font "HackGen35 Console NF"
 config.color_scheme = 'Tokyo Night'
 config.font_size = 18
