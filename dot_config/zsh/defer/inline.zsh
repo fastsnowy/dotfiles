e8d0090c9688cf61810c4b4d6b08691b3dd7e00c
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-12-21 18:46:59 +0900

    ⚡️ Remove unused eval command

--- dot_config/zsh/defer/inline.zsh
+++ dot_config/zsh/defer/inline.zsh
@@ -1,8 +1,6 @@
-eval ". $HOME/.rye/env"
 eval "$(mise activate zsh)"
 eval "$(zabrze init --bind-keys)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
 eval "$(gh completion -s zsh)"
-
