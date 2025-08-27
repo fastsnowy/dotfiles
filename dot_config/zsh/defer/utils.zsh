24585999eef1abf787835fb04c6389657e392bd2
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2025-08-28 02:37:12 +0900

    ⚡️ Bind key

--- dot_config/zsh/defer/utils.zsh
+++ dot_config/zsh/defer/utils.zsh
@@ -12,3 +12,24 @@ function fzf-bat() {
 function broot-bat() {
     broot --outcmd 'fzf --preview "bat --style=numbers --color=always --line-range :500 {}"'
 }
+
+function ghq-fzf() {
+    local selected=$(ghq list | fzf \
+        --height 80% \
+        --layout=reverse \
+        --border \
+        --preview 'glow $(ghq root)/$(echo {})/README.md 2>/dev/null || echo "No README.md found"' \
+        --preview-window=right:60% \
+        --ansi)
+    
+    if [ -n "$selected" ]; then
+        cd "$(ghq root)/$selected"
+    fi
+}
+
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
+
+# bind key
+zle -N zi
+bindkey '^z' zi
